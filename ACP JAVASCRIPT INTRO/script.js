b3264d683c30350544d05d6b4398f989cbf5513f
Davidbeshiunim <santusandor@gmail.com>
2024-12-16 19:32:30 +0000

    ACP Javascript intro

---
+++ ACP JAVASCRIPT INTRO/script.js
@@ -0,0 +1,17 @@
+document.getElementById("myheading").innerHTML = "Heading from Javascript";//statement 1//
+
+document.getElementById("myparagraph").innerHTML = "Heading from Javascript";//statement 2//
+
+// Variables: A box box that holds/stores value
+var num1 = 10;
+var num2 = 8;
+var add_result = num1 + num2;
+document.getElementById("add").innerHTML = add_result;
+
+var num3 = 50;
+var num4 = 45;
+document.getElementById("subtract").innerHTML = num3 - num4;
+
+var num5 = 30;
+var num6 = 25;
+document.getElementById("multiplication").innerHTML = num5 * num6;
