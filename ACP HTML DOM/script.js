df1deccd0f42c04d0e7aea9be5687f7a5ec91cce
Davidbeshiunim <santusandor@gmail.com>
2024-12-30 19:44:01 +0000

    PEOPLE HATE RAINFALL

---
+++ ACP HTML DOM/script.js
@@ -0,0 +1,10 @@
+
+var heading = document.getElementById("myheading").innerHTML;
+document.write(heading);
+
+function getSurprise() {
+    document.getElementById("surprise").innerHTML = "🤬 HATRED";
+    document.getElementById("surprise").style.color = red;
+}
+
+document.body.background = azure;
